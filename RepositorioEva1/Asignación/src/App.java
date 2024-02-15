4741652316d59a1585dd72ae7098590d4ae1042b
Rodaaq69 <122701094+Rodaaq69@users.noreply.github.com>
2024-02-15 16:52:56 -0600

    Add files via upload

---
+++ RepositorioEva1/Asignación/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int age=21;//Un solo "=" significa asignar
+        String carrera = "Ingenieria en sistemas";
+        String name="Rodrigo Alarcón";
+        System.out.print("Tu nombre es "+name+", Tu edad es "+age+", Tu carrera es "+carrera);
+        
+    }
+}
